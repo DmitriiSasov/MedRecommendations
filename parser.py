cbb4617afcba7f435d4ed996441ec8c6c08444fa
DmitriiSasov <s-dima29@yandex.ru>
2020-07-12 17:21:16 +0400

    Добавлена функция по считыванию МКБ кодов

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
-
+import time
 URL = 'http://cr.rosminzdrav.ru/#!/'
 
 
@@ -29,8 +29,9 @@ def go_to_recommendation_page(browser, nosology_id):
         return False
 
     search_area.send_keys(nosology_id[:len(nosology_id) - 1])
+    time.sleep(1)
     search_area.send_keys(nosology_id[len(nosology_id) - 1])
-    browser.implicitly_wait(3)
+    time.sleep(3)
 
     try:
         search_result = browser.find_elements_by_class_name('main-menu__search-result-item-text')
@@ -44,8 +45,28 @@ def go_to_recommendation_page(browser, nosology_id):
     WebDriverWait(browser, 10).until(ec.visibility_of_element_located((By.ID, "mkb")))
     print(newHref)
 
+#browser - webdriver на котором открыта страница с документом, с которого можно считать MKB
+#return - список кодов МКБ
+def get_MKBs(browser):
+
+    try:
+        mkbs = browser.find_element_by_id('mkb')
+    except NoSuchElementException:
+        print("Коды МКБ отсутствуют")
+        return []
+
+    return str(mkbs.text).split('/')
+
+#browser - webdriver на котором открыта страница с документом, с которого можно считать MKB
+#return - объект Recommendation
+def get_recommdendation_info(browser):
+    recommendation = Recommendation()
+    recommendation.MKBs = get_MKBs(browser)
+    print(recommendation.MKBs)
+
 
 browser = webdriver.Chrome('chromedriver.exe')
 go_to_recommendation_page(browser, 'e10')
+get_recommdendation_info(browser)
 
 
