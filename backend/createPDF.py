42df43bc537c8ecd9269640b8b7697e4b4cebf43
Danila Artemov <gor.ros@yandex.ru>
2020-07-13 22:37:27 +0400

    Сделал функции для формирования тезиса, диагностики, лечения, критериев. Изменил главную функцию создания PDF, используя маленькие функции.

--- backend/createPDF.py
+++ backend/createPDF.py
@@ -1,5 +1,6 @@
 import os
 from fpdf import FPDF
+import re
 
 # А - 0, 176, 80
 # B - 147, 208, 125
@@ -12,60 +13,142 @@ from fpdf import FPDF
 # 5 - 68, 84, 106
 # 5 (мой вариант) - 100, 117, 140
 
+pdf = FPDF(orientation='P', unit='mm', format='A4')
 
-def create_pdf():
-    pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+def create_pdf(recommendation):
     pdf.add_font('times', '', 'times.ttf', uni=True)
     pdf.add_font('timesbd', '', 'timesbd.ttf', uni=True)
     pdf.add_font('timesi', '', 'timesi.ttf', uni=True)
 
     pdf.add_page()
 
-    pdf.set_font('timesbd', size=18)
+    pdf.set_font('timesbd', size=22)
     pdf.cell(200, 10, txt='Оглавление', ln=1)
-    pdf.set_font('times', size=16)
+    pdf.set_font('times', size=18)
     pdf.cell(200, 10, txt='• Диагностика', ln=1)
     pdf.cell(200, 10, txt='• Лечение', ln=1)
     pdf.cell(200, 10, txt='• Критерии оценки качества медицинской помощи', ln=1)
 
     pdf.add_page()
 
-    pdf.set_font('timesbd', size=18)
-    pdf.cell(200, 10, txt='1. Диагностика', ln=1)
-    pdf.cell(200, 5, txt='', ln=1)
-    pdf.set_font('timesbd', size=16)
-    pdf.cell(200, 10, txt='1.1 Физикальное обследование:', ln=1)
-    pdf.set_font('times', size=16)
-    pdf.cell(200, 5, txt='', ln=1)
-    pdf.multi_cell(0, 5, txt='• Всем пациентам с АГ рекомендуется пальпировать пульс в покое для измерения его '
-                             'частоты и ритмичности с целью выявления аритмий [21; 32; 43].')
-    pdf.cell(200, 5, txt='', ln=1)
+    make_diagnostics(recommendation.diagnosticTheses)
+    pdf.cell(200, 10, txt='', ln=1)
+    make_treatment(recommendation.treatmentTheses)
+    pdf.cell(200, 10, txt='', ln=1)
 
-    pdf.set_font('timesbd', size=16)
-    pdf.cell(47, 8, txt='(УУР В, УДД 2) = ')
-    pdf.set_fill_color(147, 208, 125)
-    pdf.cell(6, 8, txt='В', fill=True, align='C')
-    pdf.set_fill_color(125, 212, 23)
-    pdf.cell(6, 8, txt='2', ln=1, fill=True, align='C')
-    pdf.cell(200, 5, txt='', ln=1)
+    pdf.set_font('timesbd', size=22)
+    pdf.cell(200, 10, txt='Критерии оценки качества медицинской помощи', ln=1)
+    pdf.cell(200, 10, txt='', ln=1)
 
-    pdf.set_font('times', size=16)
-    pdf.multi_cell(0, 5, txt='• Всем пациентам с АГ рекомендуется определение антропометрических данных для выявления '
-                             'избыточной массы тела/ожирения, оценка неврологического статуса и когнитивной функции, '
-                             'исследование глазного дна для выявления гипертонической ретинопатии, пальпация и '
-                             'аускультация сердца и сонных артерий, пальпация и аускультация периферических артерий '
-                             'для выявления патологических шумов, сравнение АД между руками хотя бы однократно [21].')
-    pdf.cell(200, 5, txt='', ln=1)
-
-    pdf.set_font('timesbd', size=16)
-    pdf.cell(47, 8, txt='(УУР С, УДД 5) = ')
-    pdf.set_fill_color(255, 192, 0)
-    pdf.cell(6, 8, txt='C', fill=True, align='C')
-    pdf.set_fill_color(100, 117, 140)
-    pdf.cell(6, 8, txt='5', ln=1, fill=True, align='C')
+    pdf.set_font('timesi', size=16)
+    pdf.cell(200, 10, txt='при сборе анамнеза:', ln=1)
+    make_criteria(recommendation.anamnesisCollectionDefects)
+    pdf.set_font('timesi', size=16)
+    pdf.cell(200, 10, txt='при обследовании пациентов:', ln=1)
+    make_criteria(recommendation.patientExaminationDefects)
+    pdf.set_font('timesi', size=16)
+    pdf.cell(200, 10, txt='при постановке диагноза:', ln=1)
+    make_criteria(recommendation.diagnosisDefects)
+    pdf.set_font('timesi', size=16)
+    pdf.cell(200, 10, txt='при проведении лечения:', ln=1)
+    make_criteria(recommendation.treatmentDefects)
+    pdf.set_font('timesi', size=16)
+    pdf.cell(200, 10, txt='при обеспечении преемственности:', ln=1)
+    make_criteria(recommendation.ensuringContinuityDefects)
 
     pdf.output('newGuide.pdf')
     os.system('newGuide.pdf')
 
 
-create_pdf()
+def make_theses(element):
+    pdf.set_font('times', size=16)
+    text_theses = '• ' + element.text[1:]
+    text_theses = text_theses.replace('й', 'й')
+    pdf.multi_cell(0, 6, txt=text_theses)
+    pdf.cell(200, 5, txt='', ln=1)
+
+    pdf.set_font('timesbd', size=16)
+    text_lcr = element.LCR
+    text_lre = element.LRE
+    text_full = '(УУР ' + element.LCR + ', УДД ' + element.LRE + ') = '
+
+    pdf.cell(47, 8, txt=text_full)
+    if text_lcr == 'A':
+        pdf.set_fill_color(0, 176, 80)
+    elif text_lcr == 'B':
+        pdf.set_fill_color(147, 208, 125)
+    else:
+        pdf.set_fill_color(255, 192, 0)
+    pdf.cell(6, 8, txt=text_lcr, fill=True, align='C')
+
+    if text_lre == '1':
+        pdf.set_fill_color(255, 192, 0)
+    elif text_lre == '2':
+        pdf.set_fill_color(125, 212, 23)
+    elif text_lre == '3':
+        pdf.set_fill_color(42, 173, 64)
+    elif text_lre == '4':
+        pdf.set_fill_color(57, 137, 127)
+    else:
+        pdf.set_fill_color(100, 117, 140)
+    pdf.cell(6, 8, txt=text_lre, ln=1, fill=True, align='C')
+
+    pdf.cell(200, 5, txt='', ln=1)
+
+
+def make_diagnostics(dictionary):
+    pdf.set_font('timesbd', size=22)
+    pdf.cell(200, 10, txt='1. Диагностика', ln=1)
+    pdf.cell(200, 10, txt='', ln=1)
+
+    num = 1
+    for key in dictionary.keys():
+        if key.find('Физикальное обследование') != -1 or key.find('Лабораторная диагностика') != -1 \
+                or key.find('Инструментальная диагностика') != -1 or key.find('Иные диагностические исследования') != -1:
+            pdf.set_font('timesbd', size=18)
+            buff = key
+            buff = re.sub(r'[^\w\s]+|[\d]+', r'', buff).strip()
+            title = '1.' + str(num) + ' ' + buff
+            pdf.cell(200, 10, txt=title, ln=1)
+
+            pdf.cell(200, 7, txt='', ln=1)
+            for element in dictionary[key]:
+                make_theses(element)
+
+            pdf.cell(200, 10, txt='', ln=1)
+            num += 1
+
+
+def make_treatment(dictionary):
+    pdf.set_font('timesbd', size=22)
+    pdf.cell(200, 10, txt='2. Лечение', ln=1)
+    pdf.cell(200, 10, txt='', ln=1)
+
+    num = 1
+    for key in dictionary.keys():
+        if key.find('Медикаментозная терапия') != -1:
+            pdf.set_font('timesbd', size=18)
+            buff = key
+            buff = re.sub(r'[^\w\s]+|[\d]+', r'', buff).strip()
+            title = '2.' + str(num) + ' ' + buff
+            pdf.cell(200, 10, txt=title, ln=1)
+
+            pdf.cell(200, 7, txt='', ln=1)
+            for element in dictionary[key]:
+                make_theses(element)
+
+            pdf.cell(200, 10, txt='', ln=1)
+            num += 1
+
+
+def make_criteria(dictionary):
+    pdf.set_font('timesi', size=16)
+    pdf.cell(200, 7, txt='', ln=1)
+    for element in dictionary:
+        text_theses = '• ' + element
+        text_theses = text_theses.replace('й', 'й')
+        pdf.multi_cell(0, 6, txt=text_theses)
+        pdf.cell(200, 5, txt='', ln=1)
+
+    pdf.cell(200, 10, txt='', ln=1)
