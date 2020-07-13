68dd65c9dc1c868d56ad92fa85f1417a703408a7
DmitriiSasov <s-dima29@yandex.ru>
2020-07-13 16:00:12 +0400

    УДД теперь можно записать как УУД и парсер распознает показатель

--- parser.py
+++ parser.py
@@ -7,17 +7,17 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 import time
-URL = 'http://cr.rosminzdrav.ru/#!/'
 
+URL = 'http://cr.rosminzdrav.ru/#!/'
 
 
 class IllegalArgumentException(Exception):
     pass
 
-#browser - webdriver
-#nosology_id - строка с идентификатором нозологии
-def go_to_recommendation_page(browser, nosology_id):
 
+# browser - webdriver
+# nosology_id - строка с идентификатором нозологии
+def go_to_recommendation_page(browser, nosology_id):
     if nosology_id == "":
         raise IllegalArgumentException("Идентификатор нозологии указан неверно")
 
@@ -45,8 +45,8 @@ def go_to_recommendation_page(browser, nosology_id):
     browser.get(newHref)
     print(newHref)
 
-def get_recommendation_page_url(browser, nosology_id):
 
+def get_recommendation_page_url(browser, nosology_id):
     if nosology_id == "":
         raise IllegalArgumentException("Идентификатор нозологии указан неверно")
 
@@ -75,10 +75,10 @@ def get_recommendation_page_url(browser, nosology_id):
     print(newHref)
     return newHref
 
-#browser - webdriver на котором открыта страница с документом, с которого можно считать MKB
-#return - список кодов МКБ
-def get_MKBs(browser):
 
+# browser - webdriver на котором открыта страница с документом, с которого можно считать MKB
+# return - список кодов МКБ
+def get_MKBs(browser):
     try:
         WebDriverWait(browser, 10).until(ec.visibility_of_element_located((By.ID, "mkb")))
         mkbs = browser.find_element_by_id('mkb')
@@ -91,8 +91,9 @@ def get_MKBs(browser):
 
     return str(mkbs.text).split('/')
 
+
 def get_LCR(text):
-    if text.__contains__("УУР") or text.__contains__("Уровень убедительности рекомендаций") or\
+    if text.__contains__("УУР") or text.__contains__("Уровень убедительности рекомендаций") or \
             text.__contains__("рекомендаций") and text.__contains__("доказательств"):
         if text.__contains__("УУР"):
             substr = text[text.find("УУР") + 3: text.find(",")]
@@ -105,12 +106,15 @@ def get_LCR(text):
     else:
         return ""
 
+
 def get_LRE(text):
     result = ""
-    if text.__contains__("УДД") or text.__contains__("уровень достоверности доказательств") or\
+    if text.__contains__("УДД") or text.__contains__("УУД") or text.__contains__("уровень достоверности доказательств") or \
             text.__contains__("рекомендаций") and text.__contains__("доказательств"):
         if text.__contains__("УДД"):
             substr = text[text.find("УДД") + 3: text.find(")")]
+        elif text.__contains__("УУД"):
+            substr = text[text.find("УУД") + 3: text.find(")")]
         else:
             substr = text[text.find("доказательств") + 13: text.find(")")]
 
@@ -119,10 +123,10 @@ def get_LRE(text):
                 result += char
     return result
 
-#browser - webdriver на котором открыта страница с документом, с которого можно считать MKB
-#return - словарь тезисов по темам
-def get_diagnosys_theses(browser):
 
+# browser - webdriver на котором открыта страница с документом, с рекомендациями
+# return - словарь тезисов по темам
+def get_diagnosys_theses(browser):
     theses_dict = {}
 
     html = browser.page_source
@@ -134,21 +138,20 @@ def get_diagnosys_theses(browser):
     if diagnosys_div is None:
         return {}
 
-    all_elements_in_diag_div = list(list(diagnosys_div.findAll(True, recursive=False))[1].findChild().findAll(True, recursive=False))
+    all_elements_in_diag_div = list(
+        list(diagnosys_div.findAll(True, recursive=False))[1].findChild().findAll(True, recursive=False))
 
     index = 0
     new_theses = None
     while index < len(all_elements_in_diag_div):
         current_element = all_elements_in_diag_div[index]
-        #count = len(current_element.findAll("li"))
         if current_element.name == "h2":
             current_header = current_element.get_text()
             if not theses_dict.keys().__contains__(current_header):
                 theses_dict[current_header] = []
-        elif current_element.name == "ul": #list(current_element.recursiveChildGenerator())
-            #inner_li_tags_count = len(current_element.findAll("li", recursive=False))
+        elif current_element.name == "ul":
             attrs_count = len(current_element.attrs.keys())
-            if attrs_count == 0: #inner_li_tags_count == 1
+            if attrs_count == 0:
                 theses_text = current_element.text
                 if new_theses is None:
                     new_theses = Theses()
@@ -167,19 +170,14 @@ def get_diagnosys_theses(browser):
                         theses_dict.get(current_header).append(new_theses)
                         new_theses = None
                 else:
-                    #new_theses.LCR = "Отсутствует"
-                    #new_theses.LRE = "Отсутствует"
-                    #theses_dict.get(current_header).append(new_theses)
                     new_theses = None
         index += 1
 
     return theses_dict
-    #находим заголовок внутри блока диагностики и передаем родительский блок
 
 
-
-#browser - webdriver на котором открыта страница с документом, с которого можно считать MKB
-#return - объект Recommendation
+# browser - webdriver на котором открыта страница с документом, с которого можно считать MKB
+# return - объект Recommendation
 def get_recommdendation_info(browser):
     recommendation = Recommendation()
     recommendation.MKBs = get_MKBs(browser)
@@ -193,13 +191,8 @@ def get_recommdendation_info(browser):
             print(element.LRE)
         print("\n")
 
-    #recommendation.diagnosticTheses = get_diagnosys_theses(browser)
-    #print(recommendation.diagnosticTheses)
-
 
 browser = webdriver.Chrome('chromedriver.exe')
-#browser.implicitly_wait(30)
-go_to_recommendation_page(browser, 'C22.0')
+browser.implicitly_wait(30)
+go_to_recommendation_page(browser, 'e10')
 get_recommdendation_info(browser)
-
-
