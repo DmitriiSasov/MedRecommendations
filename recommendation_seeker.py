e23b0064673287b1da8b9ce933598e3a63630664
DmitriiSasov <s-dima29@yandex.ru>
2021-04-16 00:49:59 +0300

    Добавлена реализация функции для нахождения тезисов в разделе лечения. Добавлен интерфейс класса для поиска тезисов

--- recommendation_seeker.py
+++ recommendation_seeker.py
@@ -3,14 +3,24 @@ import time
 import requests
 import json
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, PageElement
 from data_structures import Recommendation
 from data_structures import Thesis
 
 
 class RecommendationSeeker:
-
     RECOMMENDATION_URL = 'https://democenter.nitrosbase.com/clinrecalg5/API.ashx?op=GetClinrec2&id=__ID&ssid=undefined'
+
+    GLOBAL_DOCUMENT_SECTIONS = ['2. Диагностика', '3. Лечение']
+    DIAGNOSIS_SECTIONS = ['Физикальное обследование', 'Лабораторные диагностические исследования',
+                          'Лабораторная диагностика', 'Инструментальные диагностические исследования',
+                          'Инструментальная диагностика', 'Иные диагностические исследования']
+    DIAGNOSIS_SECTIONS_TO_MY_DOC_SECTIONS = {'Физикальное обследование': 'Физикальное обследование',
+                                             'Лабораторные диагностические исследования': 'Лабораторная диагностика',
+                                             'Лабораторная диагностика': 'Лабораторная диагностика',
+                                             'Инструментальные диагностические исследования': 'Инструментальная диагностика',
+                                             'Инструментальная диагностика': 'Инструментальная диагностика',
+                                             'Иные диагностические исследования': 'Иные диагностические исследования'}
 
     __recommendation_content_json = None
 
@@ -23,19 +33,66 @@ class RecommendationSeeker:
         recommendation.MKBs = self.__find_mkbs()
         recommendation.nozology_name = self.__recommendation_content_json['name']
         recommendation.table_tag = self.__find_criteria()
-        recommendation.diagnosticTheses = self.__find_diagnosys_theses()
+        recommendation.diagnosticTheses = self.__find_diagnosis_theses()
         recommendation.treatmentTheses = self.__find_treatment_theses()
 
-
     def __find_treatment_theses(self):
+        theses = []
         for section in self.__recommendation_content_json['obj']['sections']:
-            if section['title'] == "3. Лечение":
+            if section['title'] == self.GLOBAL_DOCUMENT_SECTIONS[1]:
                 html_parser = BeautifulSoup(section["content"], 'html.parser')
+                all_headers = [html_parser.find_all('h1'), html_parser.find_all('h2'), html_parser.find_all('h3')]
+                medical_treatment_tag = None
+                for header_group in all_headers:
+                    for header in header_group:
+                        if bool(re.search('(((Медикаментозное)|(Лекарственное)) лечение)|(((Медикаментозная)|(Лекарственная)) терапия)',
+                                header.text)):
+                            medical_treatment_tag = header
+                            next_tag = header.next_element
+                            thesis_seeker = self.ThesisSeeker(next_tag)
+                            while not next_tag.name.__contains__('h1') and not next_tag.name.__contains__('h2') \
+                                    and not next_tag.name.__contains__('h3'):
+                                if thesis_seeker.contains_thesis():
+                                    theses.append(thesis_seeker.extract_thesis())
+                                next_tag = next_tag.next_element
+                                thesis_seeker.set_tag(next_tag)
+        return theses
 
-                return
+    class ThesisSeeker:
 
-    def __find_diagnosys_theses(self):
-        return
+        tag = None
+
+        def __init__(self, tag: PageElement):
+            self.tag = tag
+
+        def set_tag(self, tag: PageElement):
+            self.tag = tag
+
+        def contains_thesis(self):
+            return False
+
+        def extract_thesis(self):
+            return False
+
+    def __is_diagnosis_block(self, title: str):
+        res = title == self.GLOBAL_DOCUMENT_SECTIONS[0]
+        for section in self.DIAGNOSIS_SECTIONS:
+            res = res or title == section
+        return res
+
+
+    def __find_diagnosis_theses(self):
+        theses = {self.DIAGNOSIS_SECTIONS[0]: [], self.DIAGNOSIS_SECTIONS[2]: [], self.DIAGNOSIS_SECTIONS[4]: [],
+                  self.DIAGNOSIS_SECTIONS[5]: []}
+        for section in self.__recommendation_content_json['obj']['sections']:
+            if self.__is_diagnosis_block(section['title']) and section["content"] is not None:
+                html_parser = BeautifulSoup(section["content"], 'html.parser')
+                if section['title'] != "2. Диагностика":
+
+                    for tag in html_parser.findAll(True, recursive=False):
+                        thesis_seeker = self.ThesisSeeker(tag)
+
+        return theses
 
     def __find_mkbs(self):
         return list(filter(lambda a: a != '', re.split(r'[^\wа-яА-Я.]', self.__recommendation_content_json['mkb'])))
@@ -48,10 +105,6 @@ class RecommendationSeeker:
                 return criteria_table
 
 
-
-
-
-            # browser - webdriver
 # nosology_id - строка с идентификатором нозологии
 def go_to_recommendation_page(browser, nosology_id):
     res = get_recommendation_page_url(browser, nosology_id)
