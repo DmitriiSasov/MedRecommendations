1d4f43c4cfc766fe5372723e6903c2b123d65a85
Дмитрий Сасов <s-dima29@yandex.ru>
2021-04-21 13:14:51 +0300

    Роуты теперь статические методы класса Route. Flask не может работать с роутами, записанными, как динамические методы класса.

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ from threading import Timer
 
 from flask import Flask, request, render_template, make_response
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import recommendation_seeker
 from Controller import RecommendationController
 from create_pdf import make_pdf
@@ -16,16 +15,18 @@ class Router:
     recommendation_controller = RecommendationController()
 
     # Отображаем домашнюю страницу
+    @staticmethod
     @app.route('/', methods=['GET'])
-    def home_page(self):
+    def home_page():
         return render_template('index.php')
 
     # Создаем документ и отображаем его на странице
+    @staticmethod
     @app.route('/search', methods=['POST'])
-    def make_recommendation(self):
+    def make_recommendation():
         search_req = request.form['search_req']
         mkbs = search_req.split("+")
-        url = self.recommendation_controller.generate_recommendation(mkbs)
+        url = Router.recommendation_controller.generate_recommendation(mkbs)
 
         return render_template('pdf.html', url=url)
 
