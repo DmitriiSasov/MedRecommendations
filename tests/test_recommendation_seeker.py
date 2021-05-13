a54bd86e9035fbc3911b82e8d935f70c031d6c2f
DmitriiSasov <s-dima29@yandex.ru>
2021-05-13 22:03:40 +0300

    Добавлены тесты на преобразование заголовков рекомендации.

--- tests/test_recommendation_seeker.py
+++ tests/test_recommendation_seeker.py
@@ -37,3 +37,18 @@ class TestParser(unittest.TestCase):
     def test_is_diagnosis_block_diagnosis_subblock(self):
         res = self.rec_seeker._RecommendationSeeker__is_diagnosis_block('2.3 Лабораторные диагностические исследования')
         self.assertTrue(res)
+
+    def test_transform_diagn_section_name_to_my_section_name_correct_name(self):
+        res = self.rec_seeker._RecommendationSeeker__transform_diagn_section_name_to_my_section_name(
+            '2.3 Лабораторные диагностические исследования')
+        self.assertTrue(res == 'Лабораторная диагностика')
+
+    def test_transform_diagn_section_name_to_my_section_name_incorrect_name_without_numbers(self):
+        res = self.rec_seeker._RecommendationSeeker__transform_diagn_section_name_to_my_section_name(
+            'Ключевые слова')
+        self.assertTrue(res == '')
+
+    def test_transform_diagn_section_name_to_my_section_name_incorrect_name_with_numbers(self):
+        res = self.rec_seeker._RecommendationSeeker__transform_diagn_section_name_to_my_section_name(
+            '1.6 Клиническая картина заболевания или состояния (группы заболеваний или состояний)')
+        self.assertTrue(res == '')
