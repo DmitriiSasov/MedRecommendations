a192de5d4f258f4da116968e0657a1e69b5a9bbd
Danila Artemov <gor.ros@yandex.ru>
2021-05-19 01:19:13 +0300

    Исправлены ошибки в работе загрузочного окна

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,29 @@
-function show_loading(theForm) {
+function show_loading() {
     const
-        search_button = document.querySelector('#search_button'),
         loader_window = document.querySelector('.loader_js'),
         overlay = document.querySelector('.overlay_js')
 
-    search_button.addEventListener('click', () => {
-        loader_window.style.display = 'flex'
-        overlay.style.display = 'block'
-    })
+    loader_window.style.display = 'flex'
+    overlay.style.display = 'block'
+
+    console.log('show')
 
     return false
 }
 
-if (window.location.href == 'http://127.0.0.1:8880/' || window.location.href == 'http://localhost:8880/')
-    document.addEventListener('DOMContentLoaded', show_loading)
+function hide_loading() {
+    const
+        loader_window = document.querySelector('.loader_js'),
+        overlay = document.querySelector('.overlay_js')
+
+    if (loader_window != null && overlay != null) {
+        loader_window.style.display = 'none'
+        overlay.style.display = 'none'
+    }
+
+    console.log('close')
+
+    return false
+}
+
+document.addEventListener('DOMContentLoaded', hide_loading)
