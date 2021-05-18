cc3f631ea212c2991221e90c011e718ca1dd3551
Danila Artemov <gor.ros@yandex.ru>
2021-05-18 20:52:35 +0300

    Добавил обработку ошибок по запросам.

--- server.py
+++ server.py
@@ -1,63 +1,44 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for, make_response
 from Controller import RecommendationController
-from apscheduler.schedulers.background import BackgroundScheduler
 from database_updater import DatabaseUpdater
-import time
-import datetime
 
 app = Flask(__name__, static_folder="static")
-scheduler = BackgroundScheduler(daemon=True)
-scheduler.start()
 
 db_updater = DatabaseUpdater()
 
 
-def db_update():
-    current_hour = time.strftime("%H", time.localtime(time.time()))
-    is_service_available = db_updater.update_recommendations()
-
-    if current_hour == "23":
-        if is_service_available is False:
-            scheduler.add_job(db_update_2, 'date', run_date=datetime.datetime.now()+datetime.timedelta(hours=1))
-        else:
-            db_updater.update_recommendations()
-
-
-def db_update_2():
-    is_service_available = db_updater.update_recommendations()
-
-    if is_service_available is False:
-        scheduler.add_job(db_update_2, 'date', run_date=datetime.datetime.now() + datetime.timedelta(hours=1))
-    else:
-        db_updater.update_recommendations()
-
-
-scheduler.add_job(db_update, 'interval', hours=1)
-
-
 class Router:
-
     recommendation_controller = RecommendationController()
+
+    MKB_ERROR_MESSAGE = 'Возможно, один или несколько кодов '\
+                        'МКБ-10 введены неверно.<br>Пожалуйста, '\
+                        'проверьте правильность написания кодов и '\
+                        'попробуйте еще раз.'
+    DB_UPDATING_ERROR_MESSAGE = 'В данный момент происходит обновление базы данных.'
 
     # Отображаем домашнюю страницу
     @staticmethod
     @app.route('/', methods=['GET'])
     def home_page():
-        return render_template('index.html')
+        return render_template('index.html', error_message=False)
 
     # Создаем документ и отображаем его на странице
     @staticmethod
     @app.route('/search', methods=['POST'])
     def make_recommendation():
+        if db_updater.is_db_updating():
+            return render_template('index.html', error_message=Router.DB_UPDATING_ERROR_MESSAGE)
+
         search_req = request.form['search_req']
         mkbs = search_req.split("+")
         url = Router.recommendation_controller.generate_recommendation(mkbs)
 
         if url is False:
-            return render_template('error.html')
+            return render_template('index.html', error_message=Router.MKB_ERROR_MESSAGE)
 
         return render_template('pdf.html', url=url)
 
 
 if __name__ == '__main__':
+    db_updater.scheduler_func()
     app.run(port=8880)
